9c9526fcd8278d4f6e715e1870833b7c2e45f747
coderxaron <schnitzelwolke@gmail.com>
2026-01-01 14:29:52 +0100

    Update java.js

--- java.js
+++ java.js
@@ -6,7 +6,7 @@ const pvbt = document.getElementById("pvbt")
 const oldgag = document.getElementById("oldgag")
 const oldpvb = document.getElementById("oldpvb")
 const last_sync_display = document.getElementById("ls")
-const datastore = "https://raw.githubusercontent.com/coderxaron/data/main/data/2025-12.json"
+const datastore = "https://raw.githubusercontent.com/coderxaron/data/main/data/2026-01.json"
 
 window.onload = async () => {
   //get Group Data from Vercel since cros acsess doesnt work ._.
